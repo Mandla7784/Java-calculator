4db54a3edb2a681e4cff52da771020be7eef5c09
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-12-23 18:55:58 +0200

    Updates

--- Calculator.java
+++ Calculator.java
@@ -12,9 +12,61 @@ public class Calculator implements ActionListener {
         JPanel panel;
 
         Font myFont = new Font("ink Free" , Font.BOLD,30);
-        double 
+        double num1 = 0 , num2 = 0 , results = 0 ;
+        char operator;
+
 
        Calculator(){
+        frame = new JFrame("Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420 , 550);
+        frame.setLayout(null);
+
+        textField = new JTextField();
+        textField.setBounds(50 , 25 , 300 , 50);
+        textField.setFont(myFont);
+        textField.setEditable(false);
+        
+        addButton = new JButton("+");
+        subButton = new JButton("-");
+        mulButton= new JButton("*");
+        diButton= new JButton("/");
+        clrButton = new JButton("clear");
+        deButton = new JButton("DEC");
+        delButton = new JButton("Delete");
+
+        functiButtons[0] = addButton;
+        functiButtons[1]= subButton;
+        functiButtons[2] = mulButton;
+        functiButtons[3] = diButton;
+        functiButtons[4] = diButton;
+        functiButtons[5] = clrButton;
+        functiButtons[6] = deButton;
+        functiButtons[7] = delButton;
+
+        for(int i = 0 ; i < 8 ; i ++){
+            functiButtons[i].addActionListener(this);
+            functiButtons[i].setFont(myFont);
+            functiButtons[i].setFocusable(false);
+
+        }
+        
+        for(int i = 0 ; i < 10 ; i ++){
+            numberButtons[i] = new JButton(String.valueOf(i));
+          numberButtons[i].setFont(myFont);
+          numberButtons[i].addActionListener(this);
+          numberButtons[i].setFocusable(false);
+
+          
+        }
+
+        delButton.setBounds(50 , 430 , 145 , 50);
+        clrButton.setBounds(205 , 430 , 245 , 50 );
+        frame.add(delButton );
+        frame.add(clrButton);
+
+        frame.add(textField);
+        frame.setVisible(true);
 
         }
     public static void main(String[] args){
