e168edc042b68ed6bf227494bf70f7f2c90629ec
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-12-23 18:35:07 +0200

    Updates

--- Calculator.java
+++ Calculator.java
@@ -2,9 +2,31 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Calculator {
+public class Calculator implements ActionListener {
+        JFrame frame;
+        JTextField textField;
+        JButton[] numberButtons = new JButton[10];
+        JButton[] functiButtons = new JButton[8];
+        JButton addButton , subButton , mulButton , diButton ;
+        JButton deButton , eqButton , delButton , clrButton ;
+        JPanel panel;
 
+        Font myFont = new Font("ink Free" , Font.BOLD,30);
+        double 
+
+       Calculator(){
+
+        }
     public static void main(String[] args){
+        Calculator calc = new Calculator();
+
+        // TODO: 
         System.out.println("Hello this is my calaulator");
     }
+    @Override 
+     public void actionPerformed(ActionEvent e){
+        // TODO: Auto-....
+
+     }
+
 }
