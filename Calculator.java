a0e0ce71af6717dc6dc8cee47b0122379ff4df90
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-12-23 18:26:01 +0200

    required modules

---
+++ Calculator.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Calculator {
+
+    public static void main(String[] args){
+        System.out.println("Hello this is my calaulator");
+    }
+}
